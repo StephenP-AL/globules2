163b9182a4d7b02d1ccb334b3dd54f576d8722ea
Stephen <4444646+StephenP-AL@users.noreply.github.com>
2023-11-11 09:55:52 -0600

    replaced background music with royalty free track

--- main.lua
+++ main.lua
@@ -1,8 +1,7 @@
 --[[
---Globules by AnPro (the Anti-Procrastinators)
+--Globules 
 --]]
 
 local composer = require("composer")
 display.setStatusBar(display.HiddenStatusBar);
-composer.gotoScene("scenemenu") -- For testing purposes; will probably go to menu first
---composer.gotoScene("scenePlayField") -- For testing purposes; will probably go to menu first
+composer.gotoScene("scenemenu") 
